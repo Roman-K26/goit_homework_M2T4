1c375480c0da46a304ebf7563d267c844b58867d
Roman-K26 <romankuspys777@gmail.com>
2023-10-26 19:50:28 +0300

    Commit 3

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import socket
 from pathlib import Path
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
+from datetime import datetime
 
 BASE_DIR = Path()
 BUFFER_SIZE = 1024
@@ -66,8 +67,10 @@ def save_data_from_form(data):
     parse_data = urllib.parse.unquote_plus(data.decode())
     try:
         parse_dict = {key: value for key, value in [el.split('=') for el in parse_data.split('&')]}
-        with open('storage/data.json', 'w', encoding='utf-8') as file:
+        parse_dict["timestamp"] = str(datetime.now())
+        with open('storage/data.json', 'a', encoding='utf-8') as file:
             json.dump(parse_dict, file, ensure_ascii=False, indent=4)
+            file.write("\n")
     except ValueError as err:
         logging.error(err)
     except OSError as err:
